924411715abc88ed3bf78cbef70c41d39ce158f3
Robbie <robbie.lisle@live.com>
2021-05-19 19:35:46 +0100

    buff debuff out sub 1

--- share/lib/config.js
+++ share/lib/config.js
@@ -74,7 +74,7 @@ const CONFIG_DEFAULT = {
         'heal.total',
         'heal.over',
         'heal.swing',
-        'etc.buff_debuff_out-raise'
+        'etc.buff_debuff_out'
       ]
     }, {
       id: 3,
@@ -131,7 +131,7 @@ const CONFIG_DEFAULT = {
     '_etc-powerdrain': 4,
     '_etc-powerheal': 4,
     '_etc-death': 2,
-    '_etc-buff_debuff_out-raise': 2
+    '_etc-buff_debuff_out': 2
   },
   color: {
     'gauge-default': '#444',
@@ -536,9 +536,7 @@ const COLUMN_INDEX = {
     powerdrain: 'powerdrain',
     powerheal: 'powerheal',
     death: 'deaths',
-    buff_debuff_out: {
-      raise: 'revives'
-    }
+    buff_debuff_out: 'buff debuff out'
   }
 }
 
